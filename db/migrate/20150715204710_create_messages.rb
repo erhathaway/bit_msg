62a53e1137b0eaa44f5df4d5f7722f5aab62c71d
Ethan Hathaway <erhathaway@gmail.com>
2015-07-20 12:59:28 -0400

    fix db problems

--- db/migrate/20150715204710_create_messages.rb
+++ db/migrate/20150715204710_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.integer :exchange_id,           null:false
-      t.string :op_return_data_raw,     null:false
-      t.string :op_return_data_decoded, null:false
+      t.integer :exchange_id,           null: false
+      t.string :op_return_data_raw,     null: false
+      t.string :op_return_data_decoded, null: false
       t.string :op_return_data_custom
       t.integer :message_tag_id
 
